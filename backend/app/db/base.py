a33515f4116bcf37e01a8d2be1c079e2de45f5a0
YouNa17 <futiany986@gmail.com>
2026-02-11 19:18:52 +0900

    DB接続

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,1 +1,8 @@
-print()
+# -----------------
+# 全テーブルの設計図の親
+# -----------------
+from sqlalchemy.orm import DeclarativeBase
+
+# このBaseを継承するとSQLAlchemyがDBテーブルであると認識する
+class Base(DeclarativeBase):
+    pass
--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,1 +1,21 @@
-print()
+# -----------------
+# DBと接続する窓口
+# -----------------
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+# 環境変数から DB の接続情報を取得
+# FIXME：無ければデフォルト値を使う
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
+)
+
+# DBとの接続装置(engine)を使って実際にDB操作するセッションを作る
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
