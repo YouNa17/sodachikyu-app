eb011857fb29a2d121cf9b669982680b0e17e59d
YouNa17 <futiany986@gmail.com>
2026-02-12 13:41:25 +0900

    DB_URLを.envに移す

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -6,11 +6,10 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # 環境変数から DB の接続情報を取得
-# FIXME：無ければデフォルト値を使う
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg2://postgres:postgres@db:5432/sodachikyu"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 # DBとの接続装置(engine)を使って実際にDB操作するセッションを作る
 engine = create_engine(DATABASE_URL)
