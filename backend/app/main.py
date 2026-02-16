01ae1f600d89e61eafca6db4a28be5c1c438116f
YouNa17 <futiany986@gmail.com>
2026-02-16 14:02:36 +0900

    CORS設定

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
 from app.db.session import engine
 from sqlalchemy import text
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routers import users, status, categories, actions, action_logs
 
 app = FastAPI()
+
+# CORS設定
+origins = [
+    "http://localhost:3000",  # Next.jsローカル
+    "http://127.0.0.1:3000",
+]
+
+# CORS機能をFastAPIに追加する
+app.add_middleware(
+    CORSMiddleware,
+    # 許可するフロントのURL
+    allow_origins=origins,
+    # 認証（Firebase tokenなど）を許可
+    allow_credentials=True,
+    # HTTPメソッド全て許可
+    allow_methods=["*"],
+    # Authorizationなどのヘッダーを許可
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
